d419c5b885ebc2b613bf392f9104dcceac7061f4
LeoEchenique <leo.echeni@gmail.com>
2022-09-20 23:03:45 -0300

    dep

--- src/routes/index.js
+++ src/routes/index.js
@@ -4,8 +4,13 @@ const Operations = require("./controllers/Operations");
 const Login = require("./controllers/login");
 const router = Router();
 
+router.get("/", (req, res) => {
+    res.status(200).send("NOICE!")
+})
+
 router.use("/auth", Login);
 router.use("/Operations", Operations);
 router.use("/Wallet", Wallet);
+
 
 module.exports = router;
